54a701c5005439efa0dd826f1b7139a0bbe133dc
John Penny <john.penny@audacia.co.uk>
2018-03-05 08:59:09 +0000

    Merged PR 670: Added an interface for commands with paging
    Added an interface for commands with paging

---
+++ src/Audacia.Commands/IPagingCommand.cs
@@ -0,0 +1,18 @@
+﻿using Audacia.Core;
+
+namespace Audacia.Commands
+{
+    public interface IPagingCommand<TPagingRequest> : ICommand
+        where TPagingRequest : PagingRequest
+    {
+        TPagingRequest PagingRequest { get; set; }
+    }
+
+    public interface IPagingCommand : IPagingCommand<PagingRequest>
+    {
+    }
+
+    public interface ISortablePagingCommand : IPagingCommand<SortablePagingRequest>
+    {
+    }
+}
