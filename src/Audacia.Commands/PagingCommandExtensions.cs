d7eca095207eb8a6e65475eed63c350e2bb18463
Richard Brown <richard.brown@audacia.co.uk>
2022-01-25 17:23:37 +0000

    Added paging command extensions from template project

--- src/Audacia.Commands/PagingCommandExtensions.cs
+++ src/Audacia.Commands/PagingCommandExtensions.cs
@@ -5,6 +5,9 @@ namespace Audacia.Commands
 {
     public static class PagingCommandExtensions
     {
+        private const int MaxPageSize = int.MaxValue;
+        private const int MinPageNumber = 0;
+
         public static int GetSkip<T>(this IPagingCommand<T> command) where T : PagingRequest
         {
             return command.PagingRequest.PageNumber * (command.PagingRequest.PageSize ?? int.MaxValue);
@@ -24,6 +27,44 @@ namespace Audacia.Commands
         public static string GetSqlSortDirection<T>(this IPagingCommand<T> command) where T : SortablePagingRequest
         {
             return command.PagingRequest.Descending ? "DESC" : "ASC";
+        }
+
+        /// <summary>
+        /// Ensures that default paging is applied.
+        /// </summary>
+        /// <param name="command">The command containing the paging request.</param>
+        /// <param name="pageSize">The page size to use.</param>
+        /// <param name="pageNumber">The page number to use.</param>
+        public static PagingRequest EnsureDefaultPaging(
+            this IPagingCommand command,
+            int pageSize = MaxPageSize,
+            int pageNumber = MinPageNumber)
+        {
+            return command?.PagingRequest ?? new PagingRequest(pageSize, pageNumber);
+        }
+
+        /// <summary>
+        /// Ensures that default paging is applied.
+        /// </summary>
+        /// <param name="command">The command containing the paging request.</param>
+        /// <param name="columnName">The column on which to sort.</param>
+        /// <param name="pageSize">The page size to use.</param>
+        /// <param name="pageNumber">The page number to use.</param>
+        /// <param name="descending">Whether or not sorting should be descending.</param>
+        public static SortablePagingRequest EnsureDefaultSortablePaging(
+            this ISortablePagingCommand command,
+            string columnName,
+            int pageSize = MaxPageSize,
+            int pageNumber = MinPageNumber,
+            bool descending = false)
+        {
+            return !string.IsNullOrWhiteSpace(command?.PagingRequest?.SortProperty)
+                ? command.PagingRequest
+                : new SortablePagingRequest(pageSize, pageNumber)
+                {
+                    SortProperty = columnName,
+                    Descending = descending
+                };
         }
     }
 }
